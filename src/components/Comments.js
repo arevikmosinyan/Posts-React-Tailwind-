be10de6558576965d124b060311f1476475c58f6
Arevik Mosinyan <aarreevviikk1@gmail.com>
2024-04-25 18:09:39 +0400

    responsive design

--- src/components/Comments.js
+++ src/components/Comments.js
@@ -1,17 +1,18 @@
-import React, { useState, useContext } from 'react';
+import React, { useState, useContext } from "react";
 import {
   COMMENT_BACKGROUND,
   BUTTON_COLOR,
   BUTTON_DISABLED,
   TEXT_COLOR,
-} from '../constants/colors';
-import useModal from '../customHooks/useModal';
-import SharedModal from './Modals/SharedModal';
-import postsContext from '../context/postsContext';
-import { v4 as uuidv4 } from 'uuid';
+  HOVER_BUTTON,
+} from "../constants/colors";
+import useModal from "../customHooks/useModal";
+import SharedModal from "./Modals/SharedModal";
+import postsContext from "../context/postsContext";
+import { v4 as uuidv4 } from "uuid";
 
 export default function Comments({ userId, postId }) {
-  const [commentText, setCommentText] = useState('');
+  const [commentText, setCommentText] = useState("");
   const [indexOfComment, setIndexOfComment] = useState();
   const [showAllComments, setShowAllComments] = useState(false);
   const { posts } = useContext(postsContext);
@@ -29,7 +30,7 @@ export default function Comments({ userId, postId }) {
         } else {
           return post;
         }
-      }),
+      })
     );
     closeModalsAndResetStates();
   }
@@ -48,13 +49,13 @@ export default function Comments({ userId, postId }) {
           return {
             ...post,
             allComments: post?.allComments.filter(
-              (comment, index) => index !== indexOfComment,
+              (comment, index) => index !== indexOfComment
             ),
           };
         } else {
           return post;
         }
-      }),
+      })
     );
     closeModalsAndResetStates();
   }
@@ -62,53 +63,59 @@ export default function Comments({ userId, postId }) {
   function closeModalsAndResetStates() {
     deleteCommentModal.closeModal();
     addCommentModal.closeModal();
-    setCommentText('');
+    setCommentText("");
   }
 
   return (
     <>
-      <div className='flex flex-col items-start w-full'>
-        <div className='flex'>
-          <div>
-            <p className='font-bold p-3'>
+      <div className="flex flex-col items-start w-full">
+        <div className="flex flex-col w-full">
+          <div className="sm:w-[80vw] md:w-[80vw] lg:w-[40vw]">
+            <p className="font-bold p-3">
               {targetPost?.allComments.length} Comments
             </p>
-            <textarea
-              className='w-30vw h-20 border border-gray-700 rounded-md p-2 '
-              placeholder='Write your comment here...'
-              value={commentText}
-              onChange={(e) => setCommentText(e.target.value)}></textarea>
           </div>
-          {addCommentModal.isOpen && (
-            <SharedModal
-              handleModalConfirm={onAddComment}
-              handleModalCancel={closeModalsAndResetStates}
-              modalText='Are you sure you want to add this comment?'
-              confirmButtonText='Yes, add'
-              cancelButtonText='No, skip'
-            />
-          )}
-          <div className=' flex ml-7 mt-7 items-center'>
-            <button
-              disabled={commentText.trim() === ''}
-              className={`px-4 py-2 ${
-                commentText.trim() === '' ? BUTTON_DISABLED : BUTTON_COLOR
-              } text-white border border-gray-500 rounded `}
-              onClick={() => addCommentModal.openModal()}>
-              Add
-            </button>
+          <div className="flex flex-row">
+            <div className="flex-3">
+              <textarea
+                className="sm:w-[80vw] md:w-[80vw] lg:w-[40vw] h-20 border border-gray-700 rounded-md p-2 "
+                placeholder="Write your comment here..."
+                value={commentText}
+                onChange={(e) => setCommentText(e.target.value)}
+              ></textarea>
+            </div>
+            {addCommentModal.isOpen && (
+              <SharedModal
+                handleModalConfirm={onAddComment}
+                handleModalCancel={closeModalsAndResetStates}
+                modalText="Are you sure you want to add this comment?"
+                confirmButtonText="Yes, add"
+                cancelButtonText="No, skip"
+              />
+            )}
+            <div className="flex-1 flex m-2 items-center">
+              <button
+                disabled={commentText.trim() === ""}
+                className={`px-4 py-2 ${
+                  commentText.trim() === "" ? BUTTON_DISABLED : BUTTON_COLOR
+                } text-white border border-gray-500 rounded hover:${HOVER_BUTTON}`}
+                onClick={() => addCommentModal.openModal()}
+              >
+                Add
+              </button>
+            </div>
           </div>
         </div>
         {deleteCommentModal.isOpen && (
           <SharedModal
             handleModalConfirm={onDeleteCommentConfirm}
             handleModalCancel={closeModalsAndResetStates}
-            modalText='Are you sure you want to delete this comment?'
-            confirmButtonText='Yes, delete'
-            cancelButtonText='No'
+            modalText="Are you sure you want to delete this comment?"
+            confirmButtonText="Yes, delete"
+            cancelButtonText="No"
           />
         )}
-        <ul className='list-none'>
+        <ul className="list-none max-w-full">
           {targetPost?.allComments?.map((comment, index) => {
             if (index >= 2 && !showAllComments) {
               return null;
@@ -116,15 +123,17 @@ export default function Comments({ userId, postId }) {
             return (
               <li
                 key={uuidv4()}
-                className={`${COMMENT_BACKGROUND} flex justify-between items-center w-30vw break-words mb-4 p-4 rounded-lg`}>
-                <p className='indent-4 text-justify'>Comment by {userId} :</p>
-                <p className='indent-4 text-justify'>{comment}</p>
+                className={`${COMMENT_BACKGROUND} my-3 flex items-center  text-justify indent-4 max-w-full break-words rounded-lg justify-between`}
+              >
+                <p className="indent-4 text-justify">Comment by {userId} :</p>
+                <p className="indent-4 text-justify">{comment}</p>
                 <button
-                  className={`mt-2 px-3 py-1 ${BUTTON_COLOR} text-white rounded `}
+                  className={`m-3 md:mt-2 px-3 py-1 ${BUTTON_COLOR} text-white rounded `}
                   onClick={() => {
                     deleteCommentModal.openModal();
                     setIndexOfComment(index);
-                  }}>
+                  }}
+                >
                   Delete
                 </button>
               </li>
@@ -133,8 +142,9 @@ export default function Comments({ userId, postId }) {
           {targetPost?.allComments.length > 2 && (
             <span
               className={`mt-2 px-3 py-1 ${TEXT_COLOR} rounded cursor-pointer underline`}
-              onClick={() => setShowAllComments(!showAllComments)}>
-              {showAllComments ? '...Less' : '...More'}
+              onClick={() => setShowAllComments(!showAllComments)}
+            >
+              {showAllComments ? "...Less" : "...More"}
             </span>
           )}
         </ul>
